df05696a280c38ed8e7bb531e8629d5bea0bcd89
manansaipi <boben.manan5@gmail.com>
2023-04-28 23:10:12 +0800

    crud to mysql

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,8 +1,8 @@
-const UsersModels = require('../models/users')
+const UsersModel = require('../models/users')
 
 const getAllUsers = async ( req, res) => {
     try {
-        const [data] = await UsersModels.getAllUsers()
+        const [data] = await UsersModel.getAllUsers()
         res.json({
             message: 'GET all users',
             data: data
@@ -15,34 +15,77 @@ const getAllUsers = async ( req, res) => {
     }
 }
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
     // console.log(req.body)
-    res.json({
-        message: 'CREATE new user success', 
-        data: req.body
-    })
+    const {body} = req
+    try {
+        await UsersModel.createNewUser(body)
+        res.json({
+            message: 'CREATE new user success', 
+            data: body
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage: error,
+        })
+    }
+    
 }
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
     const {id} = req.params
+    const {body} = req
     console.log('id: ', id) 
     // console.log(req.params) params -> parameter
-    res.json({
+    try {
+        await UsersModel.updateUser(body, id)
+       res.json({
         message : "UPDATE user success",
-        data: req.body
-    })
-}
-
-const deleteUser = (req, res) => {
-    const {id} = req.params
-    console.log('id: ', id) 
-    res.json({
-        message : "DELETE user success",
         data: {
             id: id,
-            name: "ben"
+            ...body
         }
-    })
+    }) 
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage: error,
+        })
+    }
+    
+    
+}
+
+const deleteUser =  async (req, res) => {
+    const {id} = req.params
+    const {body} = req
+    console.log('id: ', id)
+    try {
+        const [data] = await UsersModel.getUser(id)
+        console.log(data)
+        if(data == ""){
+            res.status(404).json({
+            message : "id not found"
+            })
+        } else{
+            await UsersModel.deleteUser(id)
+            res.json({
+            message : "DELETE user success",
+            data: {
+                id: id,
+                data: body
+                }
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage: error,
+        })    
+    }
+
+    
 }
 module.exports = {
     getAllUsers, 
--- src/models/users.js
+++ src/models/users.js
@@ -1,10 +1,36 @@
 const dbPool = require('../config/database')
 
+
+
 const getAllUsers = () => {
     const SQLQuery = 'SELECT * FROM users'
     return dbPool.execute(SQLQuery)
 }
 
+const getUser = (id) => {
+    const SQLQuery = `SELECT * FROM users WHERE id = '${id}'`
+    return dbPool.execute(SQLQuery)
+}
+
+const createNewUser = (body) => {
+    const SQLQuery = `INSERT INTO users (name, username, email) VALUES ('${body.name}', '${body.username}', '${body.email}')`
+    return dbPool.execute(SQLQuery)
+}
+
+const updateUser = (body, id) => {
+    const SQLQuery = `UPDATE users SET name = '${body.name}', username = '${body.username}', email = '${body.email}' WHERE id = '${id}'`
+    return dbPool.execute(SQLQuery)
+}
+const deleteUser = (id) => {
+    const SQLQuery = `DELETE FROM users WHERE id = '${id}'`
+    return dbPool.execute(SQLQuery)
+}
+
+
 module.exports = {
     getAllUsers,
+    getUser,
+    createNewUser,
+    updateUser,
+    deleteUser,
 }
