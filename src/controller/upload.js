a9230955ecb5eeba556fc01c0b1f99716892136d
manansaipi <boben.manan5@gmail.com>
2023-05-01 10:35:26 +0800

    delete file in local directory and public acces image

--- src/controller/upload.js
+++ src/controller/upload.js
@@ -4,26 +4,59 @@ const { Storage } = require('@google-cloud/storage');
 
 //credentials-key
 const storage = new Storage({
-  projectId: 'qwiklabs-gcp-03-da615e512362',
+  projectId: 'qwiklabs-gcp-00-d947baf6112d',
   keyFilename: './credentials/credentials.json', 
 });
 
-const bucket = storage.bucket('bucket_express2'); //my bucket name in the cloud
+const bucketName = 'bucket_express3'
+
+const bucket = storage.bucket(bucketName); //my bucket name in the cloud
 
 
-const uploadPhoto = (req, res, next) => {
+
+const uploadPhoto = async (req, res, next) => {
     const file = req.file;
     const destination = bucket.file(file.filename);// path to save in the bucket and the file name
+
+    const filePath = `./public/images/${file.filename}`;
+
+    // Get the public URL
+    const publicUrl = `https://storage.googleapis.com/${bucketName}/${file.filename}`;
+
     try {
+    const options = {
+      metadata: {
+        contentType: file.mimetype,
+      },
+      predefinedAcl: 'publicRead', // set public access control
+    };
     
-    fs.createReadStream(`./public/images/${file.filename}`)
-    .pipe(destination.createWriteStream())
- 
+    // fs.createReadStream(`./public/images/${file.filename}`)
+    // .pipe(destination.createWriteStream(options))
+
+    await new Promise((resolve, reject) => {
+      const readStream = fs.createReadStream(`./public/images/${file.filename}`);
+      const writeStream = destination.createWriteStream(options);
+
+      readStream.on('error', reject);
+      writeStream.on('error', reject);
+      writeStream.on('finish', resolve);
+
+      readStream.pipe(writeStream);
+    });
     // do delete file here in public/images directory 
+     fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error(`Failed to delete file: ${err}`);
+        }
+     })
 
     res.json({
-        message: "UPLOAD Success"
-    })
+      message: 'Upload success',
+      data: req.file,
+      url: publicUrl
+    
+    });
  
 
     } catch (error) {
